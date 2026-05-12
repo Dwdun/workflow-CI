d4e51c95040df09710fd0dc8c9223dd505c8e674
Muhammad Faqih Shiam <faqihshiam23@gmail.com>
2026-05-12 23:16:45 +0700

    update modelling

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,22 +1,23 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-import mlflow
-import mlflow.sklearn
+from sklearn.metrics import accuracy_score
 
-mlflow.set_tracking_uri("http://127.0.0.1:5000")
-mlflow.set_experiment("Students_Performance")
-
+# Load data preprocessed
 df = pd.read_csv("students_clean.csv")
 X = df.drop("MathPass", axis=1)
 y = df["MathPass"]
 
+# Split data
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-mlflow.sklearn.autolog()
+# Latih model
+model = RandomForestClassifier(random_state=42)
+model.fit(X_train, y_train)
 
-with mlflow.start_run():
-    model = RandomForestClassifier(random_state=42)
-    model.fit(X_train, y_train)
+# Evaluasi sederhana (opsional)
+y_pred = model.predict(X_test)
+acc = accuracy_score(y_test, y_pred)
+print(f"Akurasi model di CI: {acc:.4f}")
